ee97f29f519cb5b9b6150a3e8d561cb4ea84dc3d
voodoocjl <voodoocjl@hotmail.com>
2023-06-25 16:44:37 +0800

    modified

--- gen_search_space.py
+++ gen_search_space.py
@@ -1,7 +1,7 @@
 import pickle
 
 search_space = []
-for i in range(2**19):
+for i in range(2**18):
     ii = bin(i)[2:]  # str
     arch = [int(j) for j in '0' * (19-len(ii)) + ii]
     search_space.append(arch)
